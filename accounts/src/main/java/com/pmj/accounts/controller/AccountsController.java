48a0edc12b663fbb177a67a3f78e2a23e942d745
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-02-10 23:06:22 +0530

    add mappers service dto for customer and account creaation

--- accounts/src/main/java/com/pmj/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/pmj/accounts/controller/AccountsController.java
@@ -1,14 +1,23 @@
 package com.pmj.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pmj.accounts.constants.AccountsConstants;
+import com.pmj.accounts.dto.CustomerDTO;
+import com.pmj.accounts.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path="/api/v1/accounts",produces = MediaType.APPLICATION_JSON_VALUE)
 public class AccountsController {
 
-    @GetMapping("/sayHello")
-    public String sayhello() {
-        return "Hi Hello";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO> createAccount(@RequestBody CustomerDTO customerDTO) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDTO(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
 }
