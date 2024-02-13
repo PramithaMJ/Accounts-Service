f6c1dfb1811fa7fc1b7cd8d21498b630cc3026b4
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-02-13 09:27:09 +0530

    delete  api created

--- accounts/src/main/java/com/pmj/accounts/repository/AccountsRepository.java
+++ accounts/src/main/java/com/pmj/accounts/repository/AccountsRepository.java
@@ -1,7 +1,9 @@
 package com.pmj.accounts.repository;
 
 import com.pmj.accounts.entity.Accounts;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,8 @@ import java.util.Optional;
 public interface AccountsRepository extends JpaRepository<Accounts,Long> {
 
     Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
 }
